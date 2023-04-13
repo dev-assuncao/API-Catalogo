f78893a113d8b89e6cb8dab41156fd933e3755dd
dev-assuncao <davisson.assuncao@protonmail.com>
2023-04-13 15:29:15 -0300

    New API studies

---
+++ APICatalogo/Context/APIContext.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class APIContext : DbContext
+    {
+
+        public APIContext(DbContextOptions<APIContext> options): base(options)
+        {
+
+        }
+
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
