6656d0867d4a4c339c377ff75a33b559adecb23a
dev-assuncao <davisson.assuncao@protonmail.com>
2023-04-19 14:54:59 -0300

    Roting, Methods async, attribute, validations, Middlewares and configurations

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalogo.Controllers
@@ -18,37 +19,69 @@ namespace APICatalogo.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
+        public async Task<ActionResult<IEnumerable<Produto>>> Get()
         {
-            var produtos = _context.Produtos.ToList();
+            var produtos = await _context.Produtos.AsNoTracking().ToListAsync();
 
             if (produtos is null) return NotFound();
 
             return produtos;
         }
 
-        [HttpGet("{id:guid}", Name= "ObterProduto")]
-        public ActionResult<Produto> Get(int id)
+        [HttpGet("{id:int:min(1)}", Name= "ObterProduto")]
+        public async Task<ActionResult<Produto>> Get(int id)
         {
-            var produto = _context.Produtos.FirstOrDefault(x => x.Id == id);
+            var produto = await _context.Produtos.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
 
             if (produto == null) return NotFound();
 
             return produto;
         }
 
-        public ActionResult Post(Produto produto)
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Produto produto)
         {
             if(!ModelState.IsValid)
             {
                 return BadRequest();
             }
 
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
+            await  _context.Produtos.AddAsync(produto);
+            await _context.SaveChangesAsync();
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
         }
 
+
+        [HttpPut("{id:int}")]
+        public async  Task<ActionResult> Put(int id, Produto produto)
+        {
+            if (produto.Id != id)
+            {
+                return BadRequest();
+            }
+
+             _context.Entry(produto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var produto = await _context.Produtos.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado");
+            }
+
+             _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return Ok(produto);
+        }
     }
 }
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
@@ -11,6 +13,7 @@ public class Produto
 
     [Required]
     [StringLength(80)]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
@@ -27,5 +30,7 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
+
+    [JsonIgnore]
     public Categoria Categoria { get; set; }
 }
